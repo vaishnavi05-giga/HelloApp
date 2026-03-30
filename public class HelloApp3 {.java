084c92ba3f36671d9e995010f22f481c11f0696c
vaishnavi05-giga <vaishnavimudragalla5@gmail.com>
2026-03-31 00:06:42 +0530

    Add files via upload

---
+++ public class HelloApp3 {.java
@@ -0,0 +1,9 @@
+public class HelloApp3S {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
+        }
+    }
